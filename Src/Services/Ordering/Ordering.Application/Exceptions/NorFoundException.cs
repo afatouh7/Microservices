8e7e70969e7d5cee71193872fbc6c06a6744d757
fatou <fatou@DESKTOP-DAFVU83>
2023-10-20 12:09:59 +0300

    add command delete

---
+++ Src/Services/Ordering/Ordering.Application/Exceptions/NorFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ordering.Application.Exceptions
+{
+    public class NorFoundException :ApplicationException
+    {
+        public NorFoundException(string name, object key) : base($"Entity \"{ name}\" ({key}) was notfound")
+        {
+
+        }
+    }
+}
--- Src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ Src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
 using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,7 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
             if (orderToUpdate == null)
             {
                 _logger.LogError("order not exist on database");
+                throw new NorFoundException(nameof(Order), request.Id);
             }
 
             _mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
